859889f085ad39fd048b73bbabb6db5e1f5d7670
0xSacul <63975424+0xSacul@users.noreply.github.com>
2023-09-05 22:41:51 +0200

    Services Update

--- components/sections/ServicesThree.js
+++ components/sections/ServicesThree.js
@@ -13,38 +13,32 @@ export default function ServicesThree() {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-4 col-md-6">
-                            <div className={`s-single-services text-center mb-30 wow fadeInUp  animated ${activeIndex == 1 ? "active" : ""}`} data-animation="fadeInUp" data-delay=".2s" onMouseEnter={() => handleMouseEnter(1)} >
+                            <div className={`s-single-services text-center wow fadeInUp animated ${activeIndex == 1 ? "active" : ""}`} data-animation="fadeInUp" data-delay=".2s" onMouseEnter={() => handleMouseEnter(1)} >
                                 <div className="services-icon mb-30">
-                                    <img src="/img/icon/se-icon1.png" alt="img" />
-                                </div>
-                                <div className="second-services-content">
-                                    <h3><Link href="/single-service">Product Design</Link></h3>
-                                    <p>I research  create breakthrough delightful ideas leading.</p>
-                                    <Link href="/single-service" className="readmore">Read More</Link>
+                                    <img src="/img/icon/se-icon1.png" alt="img" className="pb-5" />
+                                    <h3>Création Logo</h3>
+                                    <p>En tant que designer passionné, mon objectif est de créer des logos exceptionnels qui incarnent l'essence de votre entreprise. Découvrez comment je peux vous aider à marquer votre présence unique sur le marché.</p>
+
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6">
-                            <div className={`s-single-services text-center mb-30 wow fadeInUp  animated ${activeIndex == 2 ? "active" : ""}`} data-animation="fadeInUp" data-delay=".4s" onMouseEnter={() => handleMouseEnter(2)} >
+                            <div className={`s-single-services text-center wow fadeInUp animated ${activeIndex == 2 ? "active" : ""}`} data-animation="fadeInUp" data-delay=".4s" onMouseEnter={() => handleMouseEnter(2)} >
                                 <div className="services-icon mb-30">
-                                    <img src="/img/icon/se-icon2.png" alt="img" />
-                                </div>
-                                <div className="second-services-content">
-                                    <h3><Link href="/single-service">Website Design</Link></h3>
-                                    <p>I research  create breakthrough delightful ideas leading.</p>
-                                    <Link href="/single-service" className="readmore">Read More</Link>
+                                    <img src="/img/icon/se-icon2.png" alt="img" className="pb-5" />
+                                    <h3>Création Publicitaire</h3>
+                                    <p>Je suis spécialisé dans la conception de fiches publicitaires, de flyers, d'affiches et bien plus encore pour mettre en avant votre entreprise.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6">
-                            <div className={`s-single-services text-center mb-30 wow fadeInUp  animated ${activeIndex == 3 ? "active" : ""}`} data-animation="fadeInUp" data-delay=".6s" onMouseEnter={() => handleMouseEnter(3)} >
+                            <div className={`s-single-services text-center wow fadeInUp animated ${activeIndex == 3 ? "active" : ""}`} data-animation="fadeInUp" data-delay=".6s" onMouseEnter={() => handleMouseEnter(3)} >
                                 <div className="services-icon mb-30">
-                                    <img src="/img/icon/se-icon3.png" alt="img" />
-                                </div>
-                                <div className="second-services-content">
-                                    <h3><Link href="/single-service">Game Development</Link></h3>
-                                    <p>I research  create breakthrough delightful ideas leading.</p>
-                                    <Link href="/single-service" className="readmore">Read More</Link>
+                                    <img src="/img/icon/se-icon3.png" alt="img" className="pb-5" />
+                                    <h3>Identité visuel</h3>
+                                    <p>Votre identité visuelle est votre empreinte digitale visuelle. Je suis là pour vous aider à créer une identité visuelle unique qui marquera les esprits et renforcera votre présence.
+                                    </p>
                                 </div>
                             </div>
                         </div>
